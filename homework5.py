807afc7a2eed712b9cb4af96ffe1efe0f38d8d4b
doctor1723 <doctor240290@mail.ru>
2024-07-18 23:19:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var=tuple_=(1, 2, False, "integer")
+print(immutable_var)
+print(immutable_var[0])
+mutable_list=[1, 2, "cherry"]
+print(mutable_list)
+mutable_list[0]="coconat"
+print(mutable_list)
